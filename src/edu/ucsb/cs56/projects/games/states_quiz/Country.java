4c6763e707d5291423dfe2bee501bab9f023adb7
Jenny Vien <jennyvien@umail.ucsb.edu>
2014-02-27 20:12:32 -0800

    Addeed javadoc to state and country classes

--- src/edu/ucsb/cs56/projects/games/states_quiz/Country.java
+++ src/edu/ucsb/cs56/projects/games/states_quiz/Country.java
@@ -5,95 +5,102 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
 /**
+ * Country represent a country such as the United States.
  * @author Zhansaya Abdikarimova
  * @author Jenny Vien
  *
  */
 
 public class Country {
-	private String name;
-	private String capital;
-	private ArrayList<State> states;
+    private String name;
+    private String capital;
+    private ArrayList<State> states;
 	
-	
-	public Country(){
-		states = new ArrayList<State>();
-		this.name = null;
-		this.capital = null;
+    /**
+     * Construtor for creating a Country object.
+     */
+    public Country(){
+	states = new ArrayList<State>();
+	this.name = null;
+	this.capital = null;
+    }
 
-	}
-
-	/**
-	 * @param name
-	 * @param capital
-	 */
-	public Country(String name, String capital){
-		states = new ArrayList<State>();
-		this.name = name;
-		this.capital = capital;
+    /**
+     * @param name name of the country
+     * @param capital the capital of the country
+     */
+    public Country(String name, String capital){
+	states = new ArrayList<State>();
+	this.name = name;
+	this.capital = capital;
 		
-	}
+    }
 	
-	/**
-	 * @return
-	 */
-	public String getName(){
-		return this.name;
-	}
+    /**
+     * @return The name of the country.
+     */
+    public String getName(){
+	return this.name;
+    }
 	
-	/**
-	 * @return
-	 */
-	public String getCapital(){
-		return this.capital;
-	}
+    /**
+     * @return The capital of the country.
+     */
+    public String getCapital(){
+	return this.capital;
+    }
 	
-	/**
-	 * @param name
-	 */
+    /**
+     * @param name The name of the country.
+     */
 	
-	public void setName(String name){
-		 this.name = name;
-	}
+    public void setName(String name){
+	this.name = name;
+    }
 	
-	/**
-	 * @param capital
-	 */
-	public void setCapital(String capital){
-		 this.capital = capital;
-	}
+    /**
+     * @param capital The capital of the ocuntry.
+     */
+    public void setCapital(String capital){
+	this.capital = capital;
+    }
 	
 	
-	/**
-	 * @param numberOfStates
-	 * @param file
-	 */
-	public void addStates(int numberOfStates, File file) throws Exception {
-		Scanner scanner = new Scanner(file);
+    /**
+     * @param numberOfStates represents the number of states in country
+     * @param file represent the file that hold all te states info.
+     */
+    public void addStates(int numberOfStates, File file) throws Exception {
+	Scanner scanner = new Scanner(file);
 		
-		String line;
-		int x;
-		int y;
+	String line;
+	int x;
+	int y;
 		
-		for(int i=0;i<numberOfStates;i++){
-			line = scanner.nextLine();
-			String[] splitted = line.split(",");
+	for(int i=0;i<numberOfStates;i++){
+	    line = scanner.nextLine();
+	    String[] splitted = line.split(",");
 			
-			x=Integer.parseInt(splitted[2]);
-			y=Integer.parseInt(splitted[3]);
-			states.add(new State(splitted[0],splitted[1],x,y));
-			//System.out.println(states.get(i));
-		}
+	    x=Integer.parseInt(splitted[2]);
+	    y=Integer.parseInt(splitted[3]);
+	    states.add(new State(splitted[0],splitted[1],x,y));
+	    //System.out.println(states.get(i));
 	}
-	
-	public ArrayList<State> getStatesArray(){
-		return states;
-	}
-	@Override
-	public String toString(){
-		return this.getName()+" "+this.getCapital();
-		
-	}
+    }
+
+    /**
+     * A list of all the states in the country.
+     */
+    public ArrayList<State> getStatesArray(){
+	return states;
+    }
+
+    /**
+     * Returns a string with name and the capital of the country. 
+     */
+    @Override
+    public String toString(){
+	return this.getName()+" "+this.getCapital();	
+    }
 }
--- src/edu/ucsb/cs56/projects/games/states_quiz/State.java
+++ src/edu/ucsb/cs56/projects/games/states_quiz/State.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs56.projects.games.states_quiz;
 
 /**
+ * State represents a state such as California and New York.
  * @author Zhansaya Abdikarimova
  * @author Jenny Vien
  *
@@ -8,76 +9,106 @@ package edu.ucsb.cs56.projects.games.states_quiz;
 
 public class State {
 	
-	private String name;
-	private String capital;
-	private int xCoord;
-	private int yCoord;
-	
-	public State(){
-		
-	}
+    private String name;
+    private String capital;
+    private int xCoord;
+    private int yCoord;
 
-	/**
-	 * @param name
-	 * @param capital
-	 * @param x
-	 * @param y
-	 */
-	public State(String name, String capital, int x, int y){
-		this.name = name;
-		this.capital = capital;
-		this.xCoord = x;
-		this.yCoord = y;
-	}
-	
-	/**
-	 * @return
-	 */
-	public String getName(){
-		return this.name;
-	}
-	
-	/**
-	 * @return
-	 */
-	public String getCapital(){
-		return this.capital;
-	}
-	
+    /**
+     * The default no-args constructor for State.
+     */
+    public State(){
+	name=null;
+	capital=null;
+	xCoord=0;
+	yCoord=0;
+    }
 
-	/**
-	 * @return
-	 */
-	public int getXCoord(){
-		return this.xCoord;
-	}
+    /**
+     * @param name name of the state.
+     * @param capital capital of the state.
+     * @param x x coordinate of the state
+     * @param y y coordinate of the state
+     */
+    public State(String name, String capital, int x, int y){
+	this.name = name;
+	this.capital = capital;
+	this.xCoord = x;
+	this.yCoord = y;
+    }
 	
-	public int getYCoord(){
-		return this.yCoord;
-	}
+    /**
+     * Getter for the state's name.
+     * @return the name of the state.
+     */
+    public String getName(){
+	return this.name;
+    }
 	
-	public void setName(String name){
-		 this.name = name;
-	}
-	
-	public void setCapital(String capital){
-		 this.capital = capital;
-	}
+    /**
+     * Getter for the state's capital.
+     * @return the capital of the state.
+     */
+    public String getCapital(){
+	return this.capital;
+    }
 	
 
-	public void setXCoord(int x){
-		 this.xCoord = x;
-	}
+    /**
+     * Getter for the x-coordinate of the state.
+     * @return the x-coordinate of the state.
+     */
+    public int getXCoord(){
+	return this.xCoord;
+    }
+
+    /**
+     * Getter fot the y-coordinate of the state
+     * @return the y-coordinate of the state
+     */
+    public int getYCoord(){
+	return this.yCoord;
+    }
 	
-	public void setYCoord(int y){
-		 this.yCoord = y;
-	}
+    /**
+     * Setter for the name of the state.
+     * @param name, the name of the state
+     */
+    public void setName(String name){
+	this.name = name;
+    }
+
+    /**
+     * Setter for the capital of the state.
+     * @param capital, the capital of the state
+     */	
+    public void setCapital(String capital){
+	this.capital = capital;
+    }
 	
-	@Override
-	public String toString(){
-		return this.getName()+" "+this.getCapital();
-		
-	}
+    /**
+     * Setter for the x-coordinate of the state.
+     * @param x, the x-coordinate of the state
+     */
+    public void setXCoord(int x){
+	this.xCoord = x;
+    }
 	
+    /**
+     * Setter for the y-coordinate of the state.
+     * @param y, the y-coordinate of the state
+     */	
+    public void setYCoord(int y){
+	this.yCoord = y;
+    }
+
+    /**
+     * Returns a string with the name and the capital of the state.
+     */
+	
+    @Override
+    public String toString(){
+	return this.getName()+" "+this.getCapital();
+    }
 	
 }
