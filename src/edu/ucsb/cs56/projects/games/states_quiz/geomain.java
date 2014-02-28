f066201edee74e2ea363446821ea7fb46e1c4e95
Saya <zhansaya@umail.ucsb.edu>
2014-02-27 20:39:46 -0800

    Update geomain.java

--- src/edu/ucsb/cs56/projects/games/states_quiz/geomain.java
+++ src/edu/ucsb/cs56/projects/games/states_quiz/geomain.java
@@ -14,21 +14,27 @@ import javax.imageio.*;
 public class geomain{
 
 	private static GUI frame;
-	private static QuestionManager QuestionManager;
+	private static QuestionManager questionManager;
 
 	public static void main (String[] args){
-		QuestionManager = new QuestionManager();		
+		questionManager = new QuestionManager();		
 		frame = new GUI();
-		QuestionManager.init();
+		questionManager.init();
 	}
 
+	/**
+	 * @return GUI frame the whole window of the game
+	 */
 	public static GUI getGUI(){
 		return frame;
 	}
+	/**
+	 * @return questionManager  
+	 */
 
 	public static QuestionManager getQuestionManager()
 	{
-		return QuestionManager;
+		return questionManager;
 	}
 
 
