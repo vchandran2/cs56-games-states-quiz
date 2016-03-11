0959781a310f1213a69b29f31878da699a3855fb
rowallen <rallen00@umail.ucsb.edu>
2016-03-10 18:36:01 -0800

    Update FrontPanel.java

--- src/edu/ucsb/cs56/projects/games/states_quiz/FrontPanel.java
+++ src/edu/ucsb/cs56/projects/games/states_quiz/FrontPanel.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 /**
+ * FrontPanel displays the game and difficulty options at game startup
  * @author Zhansaya Abdikarimova
  * @author Jenny Vien
  * @author Nick Eidler
