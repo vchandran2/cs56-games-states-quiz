a87da4e624ab54e8a76d310c32297fa98d003645
Veena Chandran <vchandran2@ymail.com>
2016-11-18 18:34:27 -0800

    added extra classes

---
+++ src/edu/ucsb/cs56/projects/games/states_quiz/Stopwatch.java
@@ -0,0 +1,75 @@
+package edu.ucsb.cs56.projects.games.states_quiz;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Duration;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+/**
+ * Creates a stopwatch to track length of game
+ *
+ * @author Vincent Wang, Veena Chandran
+ */
+public class Stopwatch {
+
+    public Stopwatch() {
+        EventQueue.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                ex.printStackTrace();
+            }
+
+            JFrame frame = new JFrame("Testing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(new StopWatchPane());
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+
+    public class StopWatchPane extends JPanel {
+
+        private JLabel label;
+        private long lastTickTime;
+        private Timer timer;
+
+        public StopWatchPane() {
+            label = new JLabel(String.format("%04d:%02d:%02d.%03d", 0, 0, 0, 0));
+
+            timer = new Timer(100, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    long runningTime = System.currentTimeMillis() - lastTickTime;
+                    Duration duration = Duration.ofMillis(runningTime);
+                    long hours = duration.toHours();
+                    duration = duration.minusHours(hours);
+                    long minutes = duration.toMinutes();
+                    duration = duration.minusMinutes(minutes);
+                    long millis = duration.toMillis();
+                    long seconds = millis / 1000;
+                    millis -= (seconds * 1000);
+                    label.setText(String.format("%04d:%02d:%02d.%03d", hours, minutes, seconds, millis));
+                }
+            });
+
+            JButton start = new JButton("Start");
+            start.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (!timer.isRunning()) {
+                        lastTickTime = System.currentTimeMillis();
+                        timer.start();
+                    }
+                }
+            });
+        }
+    }
+}
